bae7b702bdea06073a2c3e5072604d13f5ecabd5
Nitesh <niteshranjankar5@gmail.com>
2024-08-01 01:33:35 +0530

    "Updated dependencies in package-lock.json and added cookieParser middleware in app.js"

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
