d4be10142d5139aa3742f5dfa2bc7a1383940247
Nitesh <niteshranjankar5@gmail.com>
2024-07-31 23:26:25 +0530

    "Updated app.js to create an express app and export it, and refactored index.js to import the app and connect to database, then start the server."

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 // require('dotenv').config;  ------------> Bad way
 import dotenv from "dotenv";
-import connectDB from './db/db.js';
+import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDb Connection failed !", error);
+  });
