bae123c4252f26cbd8d72a396bf64f87fbe44e51
Nitesh <niteshranjankar5@gmail.com>
2024-08-01 19:44:56 +0530

    "Added generateAccessToken and generateRefreshToken methods to user model, and refactored pre-save hook for password hashing."

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -2,7 +2,6 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 const userSchema = new Schema(
   {
     username: {
@@ -59,14 +58,43 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save",async function (next) {
-  if(! this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
-}
+};
+
+userSchema.methods.generateAccessToken = function () {
+  const token = jwt.sign(
+    {
+      id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    }
+  );
+  return token;
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  const token = jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    }
+  );
+  return token;
+};
 
 export const User = mongoose.model("User", userSchema);
