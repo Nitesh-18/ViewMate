e0ff265c4977d86aba9f2dd3cfab8ca92b31c00f
Nitesh <niteshranjankar5@gmail.com>
2024-08-02 00:00:20 +0530

    "Added user routes and controller, updated asyncHandler, and imported routes in app.js"

--- src/app.js
+++ src/app.js
@@ -14,4 +14,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//Importing Routes
+import userRouter from './routes/user-routes.js';
+
+//Routes Declaration
+app.use('/api/v1/users',userRouter);
+
 export { app };
--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,7 +1,9 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req,res) => {
     res.status(200).json({
-        message: "User registered successfully"
+        message: "User registered successfully !"
     });
 });
+
+export {registerUser};
