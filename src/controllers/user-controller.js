5d058c894f3ba524ac9d854170d2690ce1f31a44
Nitesh <niteshranjankar5@gmail.com>
2024-08-01 23:37:10 +0530

    Added user-controller

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,7 @@
+import { asyncHandler } from "../utils/asyncHandler";
+
+const registerUser = asyncHandler(async (req,res) => {
+    res.status(200).json({
+        message: "User registered successfully"
+    });
+});
