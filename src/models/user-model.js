26fa2eadbfc15923ee5853c27ce3790000fc4b4f
Nitesh <niteshranjankar5@gmail.com>
2024-08-01 19:33:57 +0530

    Used Pre,plugins

--- src/models/user-model.js
+++ src/models/user-model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 const userSchema = new Schema(
   {
@@ -55,4 +58,15 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save",async function (next) {
+  if(! this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+}
+
 export const User = mongoose.model("User", userSchema);
