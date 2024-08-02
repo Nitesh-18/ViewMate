700506b25205ae641d7e9957140d4640f17f20b1
Nitesh <niteshranjankar5@gmail.com>
2024-08-02 23:47:31 +0530

    "Added user existence check and cloudinary image upload to user registration"

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,5 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user-model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   const { fullname, username, email, password } = req.body;
@@ -9,6 +11,19 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "Please fill in all fields");
   }
+  const existingUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existingUser) {
+    throw new ApiError(409, "Username or Email already exist !");
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) throw new ApiError(400, "Avatar Image is required ");
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  
 });
 
 export { registerUser };
