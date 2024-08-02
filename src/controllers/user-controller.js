ad45b71b635ed76cfb5ab36c8a1d26f786aa96a5
Nitesh <niteshranjankar5@gmail.com>
2024-08-02 22:21:33 +0530

    "Updated user controller and routes: added input validation and file upload middleware to register user endpoint"

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,9 +1,14 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
-const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message: "User registered successfully !"
-    });
+const registerUser = asyncHandler(async (req, res) => {
+  const { fullname, username, email, password } = req.body;
+  console.log("username:", username);
+  if (
+    [fullname, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "Please fill in all fields");
+  }
 });
 
-export {registerUser};
+export { registerUser };
